bda50d00edd89490fe8476b8c7637142795ffdf2
ricardo-faria <ricardo.jonas.faria@gmail.com>
2015-11-20 14:02:21 -0200

    big fixed

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Economy/javascript/makeGraphic.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Economy/javascript/makeGraphic.js
@@ -195,9 +195,11 @@ var MakeGraphic = {
     
     getDataForTable: function(json) {    	
     	var records = [];
+    	
         for (var j in json) {
             records.push({recid: j, categoria: json[j].nameCat, descricao: json[j].description , valor: json[j].value, data: new Date(json[j].date).toLocaleString().split(" ")[0] });
         }
+        records.push({summary: true, recid: '', categoria: '<span style="float: right;font-size: 20px;">Saldo</span>', descricao: '65151'});
        return records;
 	},
 	
@@ -238,8 +240,10 @@ var MakeGraphic = {
 	            onSubmit: function (event) {
 	                w2alert('save');
 	            },
-	            records: [   ]
+	            records: [  
+	                  ]
 	        });
+	        
 	        w2ui['table'].clear();
 	        w2ui['table'].add(MakeGraphic.getDataForTable(json));
 	        
